3a7ba5cbd244dc63843015b4728544fcde24d342
jose <joserosas461@gmail.com>
2023-03-17 00:28:40 -0400

    Finished with Hash Tables

--- ds/hash/hash.js
+++ ds/hash/hash.js
@@ -8,31 +8,28 @@ class Hash {
    * @param {*} size
    * @param {*} comparatorFunction
    */
-  constructor(size = hashTableSize, comparatorFunction) {
-    this.hashTableSize = size;
-    this.bucketList = Array(this.hashTableSize).fill(
-      new LinkedList(comparatorFunction)
-    );
-
+  constructor(size = hashTableSize, comparatorFunction, generator) {
+    this.size = size;
+    this.bucketList = Array(this.size)
+      .fill(null)
+      .map(() => new LinkedList(comparatorFunction));
+    this.generator = generator || this.defaultGen;
     this.key = {};
   }
 
   /**
-   * 
-   * @param {*} key 
-   * @param {*} generator: Callback Function for other generator 
-   * @returns 
+   *
+   * @param {*} key
+   * @returns
    */
-  hash(key,generator){
-    const hashValue = generator || this.defaultGen;
-
-    return hashValue(key);
+  hashGen(key) {
+    return this.generator(key);
   }
-  
+
   /**
-   * 
-   * @param {*} key 
-   * @returns 
+   * Adds all Char Ascii value of the key to generate an hashIndex
+   * @param {*} key
+   * @returns
    */
   defaultGen(key) {
     const hash = Array.from(key).reduce(
@@ -40,12 +37,72 @@ class Hash {
       0
     );
 
-    return hash !== null ? hash % this.bucketList : null;
+    return hash % this.size;
   }
 
-  
+  /**
+   *
+   * @param {*} key
+   * @param {*} value
+   */
+  insert(key, value) {
+    const hashIndex = this.hashGen(key);
+
+    this.key[key] = hashIndex;
+    const node = this.bucketList[hashIndex].find({
+      callback: (nValue) => nValue.key === key,
+    });
+    if (!node) {
+      this.bucketList[hashIndex].append({ key, value });
+    } else {
+      node.value.value = value;
+    }
+  }
+
+  /**
+   *
+   * @param {*} key
+   */
+  get(key) {
+    const hashIndex = this.key[key];
+    if (!hashIndex) {
+      return undefined;
+    }
+    const node = this.bucketList[hashIndex].find({
+      callback: (nodeValue) => nodeValue.key === key,
+    });
+    return node ? node.value.value : undefined;
+  }
+
+  delete(key) {
+    const hashIndex = this.key[key];
+    if (!hashIndex) {
+      return undefined;
+    }
+    const ll = this.bucketList[hashIndex];
+    const node = ll.find({ callback: (value) => value.key === key });
+    return ll.delete(node.value);
+  }
 }
 
-const arr = Array.from('jose').reduce((a, b) => a + b.charCodeAt(0), 0);
+const a = new Hash(
+  (size = 10),
+  (comparatorFunction = (a, b) => {
+    if (a.key === b.key) {
+      return 0;
+    }
+    return a.key < b.key ? -1 : 1;
+  })
+);
 
-console.log(arr);
+a.insert('Key', 'hello');
+a.insert('H', 123);
+a.insert('E', 345);
+a.insert('Y', 'jsjs');
+a.insert('K', 1234);
+
+console.log(a.get('Key'));
+console.log(a.get('H'));
+console.log(a.get('E'));
+a.delete('E');
+console.log(a.get('E'));
